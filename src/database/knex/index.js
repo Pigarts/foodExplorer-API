b6fd62b7f11d4f099e0ce46dbeeae657429a496c
Levi <Levisantiago151@gmail.com>
2023-12-05 01:07:05 -0300

    bugFix 0.02 imports renamed

--- src/database/knex/index.js
+++ src/database/knex/index.js
@@ -1,4 +1,4 @@
-const config = require("../../../knexFile");
+const config = require("../../../knexfile");
 const knex = require("knex");
 const connection = knex(config.development);
 module.exports = connection;
